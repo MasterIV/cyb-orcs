ef9a92aae2781993e2ff7938bb8a738a5826a9f1
Tobias Rojahn <tobias.rojahn@innogames.com>
2016-01-30 19:52:13 +0100

    firefox fix

--- js/basic/image.js
+++ js/basic/image.js
@@ -11,7 +11,8 @@ define(['core/graphic', 'geo/v2', 'basic/entity'],
 
 			ImageEntity.prototype.onDraw = function(ctx) {
 				ctx.globalAlpha = this.alpha;
-				ctx.drawImage(this.img, 0, 0, this.size.x|0, this.size.y|0, 0, 0, (this.size.x*this.scale)|0, (this.size.y*this.scale)|0);
+				if(this.size.x >= 1 && this.size.y >= 1)
+					ctx.drawImage(this.img, 0, 0, this.size.x | 0, this.size.y | 0, 0, 0, (this.size.x * this.scale) | 0, (this.size.y * this.scale) | 0);
 				ctx.globalAlpha = 1;
 			};
 

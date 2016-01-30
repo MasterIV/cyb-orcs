608dcb5b3696eae5ce6091eecdaa624b8b3ff00f
Felix Wagner <Armag3ddon@web.de>
2016-01-30 17:47:24 +0100

    Added time HUD

--- js/entity/hud.js
+++ js/entity/hud.js
@@ -1,4 +1,4 @@
-define(['basic/entity', 'geo/v2', 'hud/buildmenu', 'hud/resources'], function(Entity, V2, BuildMenu, Resources) {
+define(['basic/entity', 'geo/v2', 'hud/buildmenu', 'hud/resources', 'hud/time'], function(Entity, V2, BuildMenu, Resources, Time) {
 	function HUD(size) {
 		Entity.call(this, Zero(), size);
 
@@ -9,6 +9,10 @@ define(['basic/entity', 'geo/v2', 'hud/buildmenu', 'hud/resources'], function(En
 		var res = new Resources(this);
 		this.add(res);
 		res.init();
+
+		var time = new Time(this);
+		this.add(time);
+		time.init();
 	}
 
 	HUD.prototype = new Entity();

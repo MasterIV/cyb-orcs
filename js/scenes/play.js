0415ecaedb028b55053e5c5536ca64b677f313e0
Tobias Rojahn <tobias.rojahn@innogames.com>
2016-01-30 17:14:15 +0100

    changed shape to add icons

--- js/scenes/play.js
+++ js/scenes/play.js
@@ -8,11 +8,11 @@ define(['lib/scene', 'lib/viewport', 'geo/v2', 'entity/map', 'entity/hud', 'basi
 
 				var map = new Map();
 
-				map.selectRoom(shapes[13]);
+				map.selectRoom(shapes[16]);
 				map.addRoom(new V2(9,9));
 
 				var add = new Button(new V2(20, 20), function() {
-					map.selectRoom(shapes[Math.floor(Math.random()*shapes.length)], null);
+					map.selectRoom(shapes[Math.floor(Math.random()*shapes.length-1)], null);
 				});
 
 				add.rect(50, 50);
@@ -20,10 +20,9 @@ define(['lib/scene', 'lib/viewport', 'geo/v2', 'entity/map', 'entity/hud', 'basi
 				var viewport = new ViewPort(true);
 				viewport.add(map);
 
-				viewport.add(new Creature(new V2(9,10), map, null));
-				viewport.add(new Creature(new V2(10,9), map, null));
 				viewport.add(new Creature(new V2(9,9), map, null));
-				viewport.add(new Creature(new V2(10,10), map, null));
+				viewport.add(new Creature(new V2(10,9), map, null));
+				viewport.add(new Creature(new V2(11,9), map, null));
 
 				viewport.dragable(true);
 

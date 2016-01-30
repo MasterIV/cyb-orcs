e9cc59ed776ff81deb19a8b99f7a179c8dad778f
Felix Wagner <Armag3ddon@web.de>
2016-01-30 13:54:31 +0100

    fix

--- js/config/scenes.js
+++ js/config/scenes.js
@@ -1,12 +1,11 @@
 define(['scenes/menu', 'scenes/credits', 'scenes/play', 'scenes/help'],
-		function (MenuScene, CreditsScene, PlayScene, HelpScene, HUDScene) {
+		function (MenuScene, CreditsScene, PlayScene, HelpScene) {
 			return {
 				init: function () {
 					this.menu = new MenuScene();
 					this.credits = new CreditsScene();
 					this.play = new PlayScene();
 					this.help = new HelpScene();
-					this.hud = new HUDScene();
 					this.default = this.menu;
 				}
 			};
